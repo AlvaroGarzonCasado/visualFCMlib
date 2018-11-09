440ce84e26f9ae389828b12c03a9ed2403ccd181
AlvaroGarzonCasado <44000105+AlvaroGarzonCasado@users.noreply.github.com>
2018-11-09 13:17:13 +0100

    Add files via upload

--- FCM/DefinicionVariables.py
+++ FCM/DefinicionVariables.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb  4 18:44:10 2014
+This program is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or (at your option) any later 
+version.
 
-@author: pcano
+This program is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-En este archivo definimos las variables que vamos a utilizar para el
-sistema experto
+You should have received a copy of the GNU General Public License along with 
+this program. If not, see http://www.gnu.org/licenses/
+
+Author: Pablo Cano Marchal
+
 """
 
 
